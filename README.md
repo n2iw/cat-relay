c58c7771b3f89d17bdfee0bfd73df0f645b34dbd
Ming Ying <yingming18@gmail.com>
2022-02-05 20:55:49 -0500

    Added config file and some instructions.

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-cat-relay
+#Cat-relay
+## About
+Cat-relay is a Ham/SWL utility, written in Python3. It synchronizes frequency between SDR++ and RUMlogNG logging software, 
+so SDR++ can act as a panadapter for your radio.
 
-# How to run
-python3 listen.py
+## Supported platforms and software
+Although Cat-relay is platform independent, the main use case is running SDR++ with RUMlogNG on MacOS. 
+It may work with other SDR software that are compatible with Hamlib/Rigctl, or other logging software that uses Dxlab Commander 
+commands. At this moment, no software other than SDR++ and RUMlogNG is tested, including Dxlab Commander. 
+
+##  How is works
+Cat-relay reads frequencies from SDR++ and RUMlogNG every 50ms (configurable), and synchronizes them if either of them changes.
+It also synchronizes mode from SDR++ to RUMlogNG. (I haven't found a way to read mode from RUMlogNG yet).
+
+## Prerequisites
+- CAT control needs to be correctly configured in RUMlogNG
+- "DxLab Suite Commander" needs to be set to "Start" in RUMLogNG
+- "Rigctl Server" needs to be configured to run in SDR++
+- Correct IP addresses for RUMlogNG and SDR++ need to be set in config.yml unless you run everything on the same computer
+- Correct ports need to be set in config.yml, unless you happened to have the same ports as mine
+
+## Dependencies
+Cat-relay uses only one 3rd party library (pyyaml). To install it, open a terminal, go to the folder that contains cat-relay, 
+and run following command:
+
+```pip3 install -r requirements.txt```
+
+## How to run
+Open a terminal, go to the folder that contains cat-relay, and run following command:
+
+```python3 listen.py```
+
+Don't close the terminal window, you can minimize it if you'd like to.
+
+## How to quit
+Open the terminal window that cat-relay is running in and press Ctrl+C.
--- listen.py
+++ listen.py
@@ -1,29 +1,51 @@
-import socket
+import os
 import sys
 import time
+import yaml
 
-from radio_info import get_radio_info
 from hamlib import HamLibClient
 from dxlab import Commander
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 13063
+HAMLIB_IP = 'SDR_IP'
+HAMLIB_PORT = 'SDR_PORT'
 
-HAMLIB_IP = '127.0.0.1'
-HAMLIB_PORT = 4532
+CMDR_IP = 'Logger_IP'
+CMDR_PORT = 'Logger_PORT'
 
-CMDR_IP = '127.0.0.1'
-CMDR_PORT = 5555
+RETRY_TIME = 'Reconnect_time'  # seconds
+SYNC_INTERVAL = 'Sync_time'  # seconds
 
-RETRY_TIME = 10  # seconds
-SYNC_INTERVAL = 0.05  # seconds
+CONFIG_FILE = 'config.yml'
+
+
+def get_parameters():
+    config = {
+        HAMLIB_IP: '127.0.0.1',
+        HAMLIB_PORT:  4532,
+
+        CMDR_IP: '127.0.0.1',
+        CMDR_PORT: 5555,
+
+        RETRY_TIME: 10,  # seconds
+        SYNC_INTERVAL: 0.05  # seconds
+    }
+
+    if os.path.isfile(CONFIG_FILE):
+        with open(CONFIG_FILE) as c_file:
+            file_config = yaml.safe_load(c_file)
+            config.update(file_config)
+            print(config)
+
+    return config
+
 
 if __name__ == '__main__':
     # reconnect every RETRY_TIME seconds, until user press Ctrl+C
+    params = get_parameters()
     while True:
         try:
-            with HamLibClient(HAMLIB_IP, HAMLIB_PORT) as hamlib:
-                with Commander(CMDR_IP, CMDR_PORT) as cmdr:
+            with HamLibClient(params[HAMLIB_IP], params[HAMLIB_PORT]) as hamlib:
+                with Commander(params[CMDR_IP], params[CMDR_PORT]) as cmdr:
                     current_freq = None
                     current_mode = None
                     while True:
@@ -38,15 +60,15 @@ if __name__ == '__main__':
                             current_freq = hl_freq
                             current_mode = hl_mode
                             cmdr.set_freq(current_freq, hl_mode)
-                        time.sleep(SYNC_INTERVAL)
+                        time.sleep(params[SYNC_INTERVAL])
         except KeyboardInterrupt as ke:
             print("\nTerminated by user.")
             sys.exit()
         except Exception as e:
             print(e)
-            print(f'Retry in {RETRY_TIME} seconds ...')
+            print(f'Retry in {params[RETRY_TIME]} seconds ...')
             print('Press Ctrl+C to exit')
-            time.sleep(RETRY_TIME)
+            time.sleep(params[RETRY_TIME])
 
 
 
