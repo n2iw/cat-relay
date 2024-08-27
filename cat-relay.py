f07102bf344410e7ea7b7f75e8d9abcfe3bdd49f
Ming Ying <yingming18@gmail.com>
2024-08-26 21:39:08 -0400

    Refactored main functions into a CatRelay class

--- cat-relay.py
+++ cat-relay.py
@@ -10,49 +10,73 @@ from radio_control.flrig import FlrigClient
 
 from config  import Parameters, DXLAB, N1MM, FLRIG
 
+class CatRelay:
+    def __init__(self, params):
+        self.cat_mode = params.get_cat_software()
+        self.cat_ip = params.get_cat_ip()
+        self.cat_port= params.get_cat_port()
+        self.radio_info_port = params.get_radio_info_port()
+        self.cat_client = self.connect_cat()
+        print(f'Cat Software connected\n')
 
-def get_logger_client(params):
-    mode = params.get_cat_software()
-    radio_control_ip = params.get_cat_ip()
-    radio_control_port = params.get_cat_port()
-    if mode == DXLAB:
-        print(f'Connecting to Commander at {radio_control_ip}:{radio_control_port}')
-        return Commander(radio_control_ip, radio_control_port)
-    elif mode == N1MM:
-        radio_info_port = params.get_radio_info_port()
-        print(f'Connecting to N1MM at {radio_control_ip}:{radio_control_port}')
-        return N1MMClient(radio_info_port, radio_control_ip, radio_control_port)
-    elif mode == FLRIG:
-        print(f'Connecting to FLRig at {radio_control_ip}:{radio_control_port}')
-        return FlrigClient(radio_control_ip, radio_control_port)
+        self.sdr_ip = params.get_sdr_ip()
+        self.sdr_port = params.get_sdr_port()
+        self.sdr_client = self.connect_sdr()
+        print(f'SDR connected.')
+
+    def __del__(self):
+        if self.cat_client:
+            self.cat_client.close()
+            self.cat_client = None
+
+        if self.sdr_client:
+            self.sdr_client.close()
+            self.sdr_client = None
+
+    def connect_sdr(self):
+        print(f'Connecting to SDR at {self.sdr_ip}:{self.sdr_port}')
+        return HamLibClient(self.sdr_ip, self.sdr_port).__enter__()
+
+    def connect_cat(self):
+        if self.cat_mode == DXLAB:
+            print(f'Connecting to Commander at {self.cat_ip}:{self.cat_port}')
+            return  Commander(self.cat_ip, self.cat_port).__enter__()
+        elif self.cat_mode == N1MM:
+            print(f'Connecting to N1MM at {self.cat_ip}:{self.cat_port}')
+            return N1MMClient(self.radio_info_port, self.cat_ip, self.cat_port).__enter__()
+        elif self.cat_mode == FLRIG:
+            print(f'Connecting to FLRig at {self.cat_ip}:{self.cat_port}')
+            return FlrigClient(self.cat_ip, self.cat_port).__enter__()
+        else:
+            message = f'Cat software "" is not supported!'
+            print(message)
+            raise Exception(message)
+
+    def sync(self):
+        radio_freq = self.cat_client.get_freq()
+        radio_mode = self.cat_client.get_mode()
+        if (radio_freq and radio_freq != self.sdr_client.get_last_freq() and isinstance(radio_freq, int)) or \
+                (radio_mode and radio_mode != self.sdr_client.get_last_mode() and isinstance(radio_mode, str)):
+            self.sdr_client.set_freq_mode(radio_freq, radio_mode)
+            print(f'Sync from radio to SDR {radio_freq}Hz {radio_mode}')
+        else:
+            sdr_freq = self.sdr_client.get_freq()
+            sdr_mode = self.sdr_client.get_mode()
+            if (sdr_freq and sdr_freq != self.cat_client.get_last_freq() and isinstance(sdr_freq, int)) or \
+                    (sdr_mode and sdr_mode != self.cat_client.get_last_mode() and isinstance(sdr_mode, str)):
+                self.cat_client.set_freq_mode(sdr_freq, sdr_mode)
+                print(f'Sync from SDR to radio {sdr_freq}Hz {sdr_mode}')
 
 if __name__ == '__main__':
     # reconnect every RETRY_TIME seconds, until user press Ctrl+C
     params = Parameters()
     while True:
         try:
-            sdr_ip = params.get_sdr_ip()
-            sdr_port = params.get_sdr_port()
-            print(f'Connecting to SDR at {sdr_ip}:{sdr_port}')
-            with HamLibClient(sdr_ip, sdr_port) as sdr:
-                print(f'SDR connected.')
-                with get_logger_client(params) as radio:
-                    print(f'Radio controlling app connected\n')
-                    while True:
-                        radio_freq = radio.get_freq()
-                        radio_mode = radio.get_mode()
-                        if (radio_freq and radio_freq != sdr.get_last_freq() and isinstance(radio_freq, int)) or \
-                           (radio_mode and radio_mode != sdr.get_last_mode() and isinstance(radio_mode, str)):
-                            sdr.set_freq_mode(radio_freq, radio_mode)
-                            print(f'Sync from radio to SDR {radio_freq}Hz {radio_mode}')
-                        else:
-                            sdr_freq = sdr.get_freq()
-                            sdr_mode = sdr.get_mode()
-                            if (sdr_freq and sdr_freq != radio.get_last_freq() and isinstance(sdr_freq, int)) or \
-                                (sdr_mode and sdr_mode != radio.get_last_mode() and isinstance(sdr_mode, str)):
-                                radio.set_freq_mode(sdr_freq, sdr_mode)
-                                print(f'Sync from SDR to radio {sdr_freq}Hz {sdr_mode}')
-                        time.sleep(params.get_sync_interval())
+            cat_relay = CatRelay(params)
+            while True:
+                cat_relay.sync()
+                time.sleep(params.get_sync_interval())
+
         except KeyboardInterrupt as ke:
             print("\nTerminated by user.")
             sys.exit()
